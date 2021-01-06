39701284ff8f489a5a64ead797b60e368b5dede7
Minh Nguyen <mnguyen0226@vt.edu>
2021-01-05 19:20:17 -0500

    Complete

--- marioRL/Adept/script/adept_main.py
+++ marioRL/Adept/script/adept_main.py
@@ -1,30 +1,14 @@
 """
     adept main script
+    -------------------------------------
     adeptRL: git clone repo on Heron Systems
     After git clone, git pull adept, do:
-     pip install -U ray[tune]
+    pip install -U ray[tune]
     conda install protobuf
-    1/ How to run the environment
-    a/ python adept_main.py --env SuperMarioBros-1-1-v0 --net3d AdeptMarioNet
-        --manager SimpleEnvManager --nb-env 2
 
-    b/ python adept_mario_script.py --agent AdeptMarioAgent --nb-env 2
+    How to run the environment
+    python adept_mario_script.py --agent AdeptMarioAgent --nb-env 2
     --env SuperMarioBros-1-1-v0 --net3d AdeptMarioNet --netbody Linear
-
-
-    IMPLEMENT:
-        env
-        replay
-        net
-        agent
-
-    * How to run Tmux?
-    ssh heron@banshee => HeronR&D
-    tmux new
-    tmux attach -t 13
-    => Run your session
-
-    Tmux -t 15: for testing purposes
 
 """
 from adept.scripts.local import parse_args, main
